e42d4244550b9b8d20030095217cd8821de79645
Rodrigo Pinto <rodrigopqn@gmail.com>
2010-07-24 16:11:55 -0300

    initial commit

---
+++ Gemfile
@@ -0,0 +1,20 @@
+source :gemcutter
+gem "rails", "2.3.8"
+
+gem 'haml'
+gem 'rcov'
+gem 'mysql'
+
+group :development do
+  # bundler requires these gems in development
+  # gem "rails-footnotes"
+  gem "ruby-debug"
+end
+
+group :test do
+  # bundler requires these gems while running tests
+  gem "rspec"
+  gem 'rspec-rails' # gem "faker"
+  gem 'webrat'
+  gem 'hpricot' 
+end
---
+++ config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = 'e6984692030bf6dfea561a4bb1997e064b824c17ed88ae797e42bd48bbbdc8206ef15ed1e14a2e939c52576f76b5ae67b470338f6791c4c08401fd5c7ceac75a';
